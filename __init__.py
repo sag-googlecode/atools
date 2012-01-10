2ed27bc8500c2c810ddf60c32b8f48ff26ea1d24
Stephen G <stephen@powerpup118.(none)>
2012-01-10 01:34:05 -0700

    Added a simple __init__.py, init file

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python -B
+# -*- coding: utf-8 -*-
+###########################
+# | . _ |_ _|_ _  _ |  _  #
+# |_|(_|| | | |_)(_)|<(/_ #
+#     _|      |           #
+###########################
+#- Atools, __init__.py is a basic module init script, so that "from atools.atools import *" works
+#- if that is so desired (helps during development sometimes, so you can git pull at any time)
+# See included "License.txt"
